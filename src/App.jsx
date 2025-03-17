10d4d1eb2a629ec751190aecee1641697aefb920
Truket Lema <truketlema9489@gmail.com>
2025-03-17 19:10:03 +0300

    Updated project with latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import React from "react";
 import UserCard from "./component/user_card"; // Uppercase import name
 
